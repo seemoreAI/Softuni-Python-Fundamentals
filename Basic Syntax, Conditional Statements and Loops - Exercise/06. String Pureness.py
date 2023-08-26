4a313bf5032c7f2fa2591131ed54b9fbe17691ea
seemoreAI <125391702+seemoreAI@users.noreply.github.com>
2023-08-26 17:19:01 +0300

    new completed tasks

--- Basic Syntax, Conditional Statements and Loops - Exercise/06. String Pureness.py
+++ Basic Syntax, Conditional Statements and Loops - Exercise/06. String Pureness.py
@@ -8,3 +8,15 @@ for _ in range(n):
             break
     else:
         print(f"{my_str} is pure.")
+
+
+# shorter way to to it :
+#
+# n = int(input())
+#
+# for _ in range(n):
+#     my_str = input()
+#     if any(char in ",._" for char in my_str):
+#         print(f"{my_str} is not pure!")
+#     else:
+#         print(f"{my_str} is pure.")
