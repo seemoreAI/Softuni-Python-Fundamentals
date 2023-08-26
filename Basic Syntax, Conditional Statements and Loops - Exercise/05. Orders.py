e70e086fa40e8fb204e1c7b09782b8663417eb02
seemoreAI <125391702+seemoreAI@users.noreply.github.com>
2023-08-26 17:05:56 +0300

    new completed tasks

--- Basic Syntax, Conditional Statements and Loops - Exercise/05. Orders.py
+++ Basic Syntax, Conditional Statements and Loops - Exercise/05. Orders.py
@@ -1,13 +1,15 @@
 number_of_orders = int(input())
-
+total_price = 0
 for _ in range(number_of_orders):
     price_of_capsule = float(input())
     days = int(input())
     capsules_per_day = int(input())
-    if not 0.01 <= price_of_capsule <= 100.00 or \
-        1 <= days <= 31 or \
-        1 <= capsules_per_day <= 2000:
+    if 0.01 <= price_of_capsule <= 100.00 and \
+            1 <= days <= 31 and \
+            1 <= capsules_per_day <= 2000:
         price = price_of_capsule * days * capsules_per_day
-        print(f"The price for the coffee is: ${price}")
+        total_price += price
+        print(f"The price for the coffee is: ${price:.2f}")
     else:
         continue
+print(f"Total: ${total_price:.2f}")
