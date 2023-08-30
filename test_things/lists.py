9e8ed4019d9223db340167993b163988cd189f82
seemoreAI <125391702+seemoreAI@users.noreply.github.com>
2023-08-30 22:16:37 +0300

    new completed tasks

--- test_things/lists.py
+++ test_things/lists.py
@@ -1,2 +1,21 @@
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+print(thislist[-4:-1])
+
 thislist = ["apple", "banana", "cherry"]
+thislist[1:2] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
 print(thislist)
